90786b6022b8391dd0e6db660885ff433f4fa67f
hassan-mb <hassan.m.behbahani@gmail.com>
2024-09-01 21:20:23 +0300

    stuck at removing the reg/login navbar and adding the logout

---
+++ src/api/storage.js
@@ -0,0 +1,12 @@
+const storeToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+const checkToken = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    return true;
+  } else return false;
+};
+
+export { storeToken, checkToken };
